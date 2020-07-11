6315c90cc8af3ebbe3768fd409a8d9522f3e5713
alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>
2020-07-11 11:54:22 -0300

    Start creating triage form

--- actions.py
+++ actions.py
@@ -101,8 +101,8 @@ class ActionStoreMood(Action):
 
         tracker.slots["mood_historic"] = mood
 
-        #dispatcher.utter_message(text=f"Your mood '{mood}' have been recorded!")
-        #return [SlotSet("mood_historic", mood)]
+        # dispatcher.utter_message(text=f"Your mood '{mood}' have been recorded!")
+        return [SlotSet("mood_historic", mood)]
 
 
 class ActionDebugBot(Action):
@@ -177,7 +177,78 @@ class DemographicForm(FormAction):
                 ),
             ],
         }
-        #print(response)
+        return(response)
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+
+        dispatcher.utter_template("utter_end_demographic", tracker)
+        return []
+
+
+class TriageForm(FormAction):
+
+    def name(self):
+        """Unique identifier of the form"""
+        return "triage_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return [
+            "anxiety_dsm",
+            "drug_dsm",
+            "depression_dsm",
+            "mania_dsm",
+            "psychosis_dsm",
+            "dissociation_dsm",
+            "suicide_dsm",
+        ]
+
+    def slot_mappings(self):
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        response = {
+            "age": [
+                self.from_entity(
+                    entity="number", intent=["inform_number"]
+                ),
+            ],
+
+            "gender": self.from_entity(entity="gender"),
+
+            "neighborhood": self.from_entity(entity="neighborhood"),
+
+            "therapy": [
+            self.from_intent(intent="affirm", value=True),
+            self.from_intent(intent="deny", value=False)
+            ],
+
+            "ethnicity": self.from_entity(entity="ethnicity"),
+
+            "work": [
+            self.from_intent(intent="affirm", value=True),
+            self.from_intent(intent="deny", value=False)
+            ],
+
+            "major": self.from_entity(entity="major"),
+
+            "timeunb": [
+                self.from_entity(
+                    entity="number", intent=["inform_number"]
+                ),
+            ],
+        }
         return(response)
 
     def submit(
